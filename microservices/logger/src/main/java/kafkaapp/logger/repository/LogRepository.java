78f3cb26be9bd2cd0a57b71d835a46905b8bb500
TibbersEvolved <salt-dev@MacBook-Pro-10.local>
2025-02-12 15:26:30 +0100

    Logger: Added boilerplate servicelayer and in memory repository

--- microservices/logger/src/main/java/kafkaapp/logger/repository/LogRepository.java
+++ microservices/logger/src/main/java/kafkaapp/logger/repository/LogRepository.java
@@ -1,7 +1,31 @@
 package kafkaapp.logger.repository;
 
+import kafkaapp.logger.models.LogItem;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class LogRepository {
+
+    private List<LogItem> logItems;
+
+    public LogRepository() {
+        this.logItems = new ArrayList<>();
+    }
+
+    public void add(LogItem logItem) {
+        this.logItems.add(logItem);
+    }
+
+    public List<LogItem> getLogItems() {
+        return logItems;
+    }
+
+    public List<LogItem> getLogItemsByService(String serviceName) {
+        return logItems.stream()
+                .filter(s -> s.getServiceName().equals(serviceName))
+                .toList();
+    }
 }
--- microservices/logger/src/main/java/kafkaapp/logger/service/LogService.java
+++ microservices/logger/src/main/java/kafkaapp/logger/service/LogService.java
@@ -1,7 +1,25 @@
 package kafkaapp.logger.service;
 
+import kafkaapp.logger.models.LogItem;
+import kafkaapp.logger.repository.LogRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LogService {
+
+    private LogRepository logRepository;
+
+    public LogService(LogRepository logRepository) {
+        this.logRepository = logRepository;
+    }
+
+    public List<LogItem> getLogs() {
+        return logRepository.getLogItems();
+    }
+
+    public List<LogItem> getLogsByServiceName(String serviceName) {
+        return logRepository.getLogItemsByService(serviceName);
+    }
 }
